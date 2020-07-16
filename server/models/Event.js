f4622f4c6469293dde4fc1b8a5c61564a8bdc641
sofi <rodriguezsofiaf@gmail.com>
2020-07-16 19:47:19 -0300

    second

---
+++ server/models/Event.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const EventSchema = mongoose.Schema({
+    city:{
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        trim: true
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    details: {
+        type: String,
+        required: true
+    },
+    creator: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User'
+    }]
+})
+
+module.exports = mongoose.model('Event', EventSchema);
---
+++ server/routes/users.js
@@ -0,0 +1,26 @@
+// routes to create users
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const { check } = require("express-validator");
+const auth = require("../middleware/auth");
+
+// Create an user
+// api/users
+router.post(
+  "/",
+  [
+    check("name", "Name is mandatory").not().isEmpty(),
+    check("username", "Username is mandatory").not().isEmpty(),
+    check("email", "Invalid email").isEmail(),
+    check("password", "Minimum length: 6 characters").isLength({ min: 6 }),
+  ],
+  userController.createUser
+);
+
+router.get("/", 
+auth,
+userController.getUsers
+ );
+
+module.exports = router;
